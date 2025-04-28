e303f676b1ead797602969356992b4bb494fc416
Bart <bartoszpudlo07@gmail.com>
2025-04-29 00:34:06 +0200

    Added a solution for decorators. need to finish them

--- main.py
+++ main.py
@@ -6,9 +6,27 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+## this is one way of adding decorators using html syntax
+# can use this solution https://stackoverflow.com/questions/70676351/is-there-a-flask-function-called-make-bold-i-am-very-confused-as-i-am-not-abl
 @app.route("/bye")
 def bye():
-    return "Bye!"
+    return "<u><em><b>Bye!</b></em></u>"
+
+## this is one way of adding decorators
+
+def make_bold(func):
+
+
+
+@app.route("/byebye")
+@make_bold
+@make_emphasis
+@make_underlined
+
+def bye():
+    return "ByeBye!"
+
+
 
 @app.route("/username/<name>/<int:number>")
 def greet(name, number):
