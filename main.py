6ffd1c4b119eed7eae3ed82379e5e03e1801b995
Bart <bartoszpudlo07@gmail.com>
2025-05-06 23:16:02 +0200

    Added a new excercise for decorators

--- main.py
+++ main.py
@@ -40,6 +40,27 @@ def contact():
 @app.route("/username/<name>/<int:number>")
 def greet(name, number):
     return f"Hello there! {name}, you are {number} years old!"
+## new excercise on how to use decorators with kwargs and args
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function()
+        return wrapper
+
+
+@is_authenticated_decorator
+
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post")
+
+new_user = User("bart")
+create_blog_post(new_user)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
